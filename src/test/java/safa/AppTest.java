4461c41fe1635e48ef964ba11599462d33d0c871
hasanussafa <45102174+hasanussafa@users.noreply.github.com>
2023-08-01 00:01:16 -0400

    Update AppTest.java

--- src/test/java/safa/AppTest.java
+++ src/test/java/safa/AppTest.java
@@ -1,19 +1,1 @@
-package safa;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
